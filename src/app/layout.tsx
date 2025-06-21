ceaa7eef04a9c38eff320c6b3a9dda0974be5dc0
taylorwaddell <Taylor.Waddell@verisglobal.com>
2025-06-21 08:00:30 -0500

    Refactor README, update dependencies, and enhance layout and styling for user experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>
+      <body className="root">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,83 @@
-import Link from "next/link";
+import { HydrateClient, api } from "~/trpc/server";
 
-import { LatestPost } from "~/app/_components/post";
+import { Avatar } from "@base-ui-components/react/avatar";
+import Link from "next/link";
 import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
+  // const hello = await api.post.hello({
+  //   text: !session ? "there" : (session.user.name ?? "there"),
+  // });
 
   if (session?.user) {
     void api.post.getLatest.prefetch();
+    console.log(session.user);
   }
+
+  const userImage = () => {
+    const user = session?.user;
+    if (user) {
+      const firstLetter = user.name ? user.name[0] : "";
+      if (user.image) {
+        return (
+          <Avatar.Root className="">
+            <Avatar.Image
+              src={user.image}
+              width="48"
+              height="48"
+              className="rounded-full"
+            />
+            <Avatar.Fallback className="rounded-full">
+              {firstLetter}
+            </Avatar.Fallback>
+          </Avatar.Root>
+        );
+      } else {
+        return (
+          <Avatar.Root className="">
+            <Avatar.Fallback className="rounded-full">
+              {firstLetter}
+            </Avatar.Fallback>
+          </Avatar.Root>
+        );
+      }
+    }
+  };
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      {session ? (
+        <main className="flex min-h-screen justify-between bg-stone-800 p-3 text-white">
+          <h1 className="text-3xl">Remembrall</h1>
+
+          <div className="flex gap-3">
+            {userImage()}
+            <Link
+              href={"/api/auth/signout"}
+              className="h-fit rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              {"Sign out"}
+            </Link>
+          </div>
+        </main>
+      ) : (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-stone-800 text-white">
+          <h1 className="text-3xl">Remembrall</h1>
+          <Link
+            href={"/api/auth/signin"}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          >
+            {"Sign in"}
+          </Link>
+        </main>
+      )}
+    </HydrateClient>
+  );
+}
+
+{
+  /* <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
@@ -52,18 +114,9 @@ export default async function Home() {
               <p className="text-center text-2xl text-white">
                 {session && <span>Logged in as {session.user?.name}</span>}
               </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
             </div>
           </div>
 
           {session?.user && <LatestPost />}
-        </div>
-      </main>
-    </HydrateClient>
-  );
+        </div> */
 }
