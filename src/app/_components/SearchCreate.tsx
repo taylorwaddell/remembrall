91f85b94df7950b03f77dc9db7b3869f76569f03
taylorwaddell <Taylor.Waddell@verisglobal.com>
2025-06-25 17:12:59 -0500

    Add loading indicator to SearchCreate component and clean up code structure.

--- src/app/_components/SearchCreate.tsx
+++ src/app/_components/SearchCreate.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowRight, Pencil, Search } from "lucide-react";
+import { ArrowRight, Loader, Pencil, Search } from "lucide-react";
 
 import { Input } from "@base-ui-components/react/input";
 import { Toggle } from "@base-ui-components/react/toggle";
@@ -11,7 +11,6 @@ import { useState, type FormEvent } from "react";
 export default function SearchCreate() {
   const [userText, setUserText] = useState("");
   const [mode, setMode] = useState<Mode>(Mode.Search);
-  const [loading, setLoading] = useState<boolean>(false);
   const [nodes, setNodes] = useState<
     {
       text: string;
@@ -20,12 +19,13 @@ export default function SearchCreate() {
   const createMemoryNode = api.memoryNode.createMemoryNode.useMutation({
     onSuccess: () => console.log("yeah bro"),
   });
-  const { refetch } = api.memoryNode.getMemoryNodesByUserId.useQuery(
-    {
-      query: userText,
-    },
-    { enabled: false },
-  );
+  const { refetch, isFetching } =
+    api.memoryNode.getMemoryNodesByUserId.useQuery(
+      {
+        query: userText,
+      },
+      { enabled: false },
+    );
   const submitNewNode = async (e: FormEvent) => {
     e.preventDefault();
     if (mode === Mode.Create) {
@@ -41,47 +41,51 @@ export default function SearchCreate() {
   };
 
   return (
-    <>
-      <form
-        className="flex h-fit w-1/3 flex-col gap-1"
-        onSubmit={(e) => submitNewNode(e)}
+    <form
+      className="flex h-fit w-1/3 flex-col gap-1"
+      onSubmit={(e) => submitNewNode(e)}
+    >
+      <ToggleGroup
+        defaultValue={[String(Mode.Search)]}
+        onValueChange={(e) => setModeState(String(e))}
+        className="flex w-fit rounded-full p-2"
       >
-        <ToggleGroup
-          defaultValue={[String(Mode.Search)]}
-          onValueChange={(e) => setModeState(String(e))}
-          className="flex w-fit rounded-full p-2"
+        <Toggle
+          value={String(Mode.Search)}
+          className="mr-2 flex items-center gap-2 rounded-full px-3 py-1 active:bg-stone-200 active:text-stone-800 data-[pressed]:bg-stone-200 data-[pressed]:text-stone-800"
         >
-          <Toggle
-            value={String(Mode.Search)}
-            className="mr-2 flex items-center gap-2 rounded-full px-3 py-1 active:bg-stone-200 active:text-stone-800 data-[pressed]:bg-stone-200 data-[pressed]:text-stone-800"
-          >
-            <Search size={16} /> Search
-          </Toggle>
-          <Toggle
-            value={String(Mode.Create)}
-            className="flex items-center gap-2 rounded-full px-3 py-1 active:bg-stone-200 active:text-stone-800 data-[pressed]:bg-stone-200 data-[pressed]:text-stone-800"
-          >
-            <Pencil size={16} /> Create
-          </Toggle>
-        </ToggleGroup>
-        <div className="flex h-fit w-full rounded-full border-1 p-2">
-          <Input
-            placeholder="search"
-            value={userText}
-            onChange={({ target }) => setUserText(target.value)}
-            className="width-full flex-1 rounded-full p-3"
-          />
-          <button type="submit" className="w-25 rounded-full p-2">
+          <Search size={16} /> Search
+        </Toggle>
+        <Toggle
+          value={String(Mode.Create)}
+          className="flex items-center gap-2 rounded-full px-3 py-1 active:bg-stone-200 active:text-stone-800 data-[pressed]:bg-stone-200 data-[pressed]:text-stone-800"
+        >
+          <Pencil size={16} /> Create
+        </Toggle>
+      </ToggleGroup>
+      <div className="flex h-fit w-full rounded-full border-1 p-2">
+        <Input
+          placeholder="search"
+          value={userText}
+          onChange={({ target }) => setUserText(target.value)}
+          className="width-full flex-1 rounded-full p-3"
+        />
+        <button type="submit" className="w-25 rounded-full p-2">
+          {isFetching ? (
+            <Loader className="mx-auto animate-spin" />
+          ) : (
             <ArrowRight className="mx-auto" />
-          </button>
-        </div>
-      </form>
-      <ul>
-        {nodes.map((n) => (
-          <li key={n.text}>{n.text}</li>
-        ))}
-      </ul>
-    </>
+          )}
+        </button>
+      </div>
+      {Boolean(nodes?.length) && (
+        <ul>
+          {nodes.map((n) => (
+            <li key={n.text}>{n.text}</li>
+          ))}
+        </ul>
+      )}
+    </form>
   );
 }
 
