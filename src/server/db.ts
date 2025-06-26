fc341be3234021cc9e114a2453debb77beff054e
taylorwaddell <Taylor.Waddell@verisglobal.com>
2025-06-26 08:04:15 -0500

    Add Prisma LibSQL adapter support and update schema configuration to enable Turso usage.

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
-import { env } from "~/env";
+const adapter = new PrismaLibSQL({
+  url: `${process.env.AUTH_TURSO_DATABASE_URL}`,
+  authToken: `${process.env.AUTH_TURSO_AUTH_TOKEN}`,
+});
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+const createPrismaClient = () => new PrismaClient({ adapter });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = createPrismaClient();
