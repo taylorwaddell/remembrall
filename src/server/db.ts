7e11f787045e8914a4f3ce5c79e63d96bde2733a
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2025-09-29 21:01:44 -0700

    Migrate to postgres (#2)
    * Refactor Prisma configuration and database connection; remove @prisma/adapter-libsql dependency and update schema to use PostgreSQL. Simplify memory node search query for improved performance.
    
    * Update .env.example for PostgreSQL configuration; add full-text search setup in PostgreSQL and create necessary migration files for MemoryNode and Tag tables. Adjust Prisma schema to include direct URL for database connection.

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,19 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-
-const adapter = new PrismaLibSQL({
-  url: `${process.env.AUTH_TURSO_DATABASE_URL}`,
-  authToken: `${process.env.AUTH_TURSO_AUTH_TOKEN}`,
-});
 
 const createPrismaClient = () => {
-  if (process.env.NODE_ENV === "production") {
-    return new PrismaClient({ adapter });
-  } else {
-    return new PrismaClient({
-      log: ["query", "error", "warn"],
-    });
-  }
+  return new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 };
 
 export const db = createPrismaClient();
