b2f77935d8292ec698a8bb98f04ad75731ff465d
taylorwaddell <Taylor.Waddell@verisglobal.com>
2025-09-29 20:49:32 -0700

    Update .env.example for PostgreSQL configuration; add full-text search setup in PostgreSQL and create necessary migration files for MemoryNode and Tag tables. Adjust Prisma schema to include direct URL for database connection.

--- src/env.js
+++ src/env.js
@@ -14,6 +14,7 @@ export const env = createEnv({
     AUTH_GOOGLE_ID: z.string(),
     AUTH_GOOGLE_SECRET: z.string(),
     DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url().optional(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -37,6 +38,7 @@ export const env = createEnv({
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
